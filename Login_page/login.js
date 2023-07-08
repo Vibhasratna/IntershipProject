30bf6a3277939f6859f1c3cd13d82a0042ecf544
Vibhas Ratna <100060723+Vibhasratna@users.noreply.github.com>
2023-07-08 16:43:23 +0530

    Add files via upload

---
+++ Login_page/login.js
@@ -0,0 +1,10 @@
+let adminBtn = document.getElementById("adminBtn");
+let facultyBtn = document.getElementById("facultyBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
+
+facultyBtn.onclick = function () {
+
+
+
+}
